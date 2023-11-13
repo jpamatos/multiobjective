9f3cb8bd27ab033f51c437ff8b0e66034a953a20
Jean Matos <jeanpierream@hotmail.com>
2023-11-13 19:04:48 -0300

    update lexicographic

--- src/genetic_algorithm.py
+++ src/genetic_algorithm.py
@@ -9,13 +9,14 @@ class GeneticAlgorithm:
         self.population = []
         self.generation = 0
         self.eval_sum = 0
-        self.best_solution = 0
+        self.best_solution = None
         self.solution_list = []
         self.X_train = X_train
         self.X_test = X_test
         self.y_train = y_train
         self.y_test = y_test
         self.pareto = []
+        self.frontier = []
 
     def init_population(self) -> None:
         for _ in range(self.population_size):
@@ -25,26 +26,26 @@ class GeneticAlgorithm:
     def sort_population(self) -> None:
         self.population = sorted(self.population, key=lambda individual: individual.eval, reverse=True)
 
-    def best_individual(self, individual: Individual) -> None:
-        if individual.eval > self.best_solution.eval:
-            self.best_solution = individual
-    
+    def best_individual(self) -> Individual:
+        maximum = max(self.pareto, key=lambda x: x.eval)
+        threshold_first = maximum.eval * 0.05
+        interval = [individual for individual in self.pareto if maximum.eval - threshold_first <= maximum.eval <= maximum.eval + threshold_first]
+        minimum = min(interval, key=lambda x:x.loss)
+        threshold_second = minimum.loss * 0.05
+        interval2 = [individual for individual in interval if minimum.loss - threshold_second <= minimum.loss <= minimum.loss + threshold_second]
+        if len(interval2) == 1:
+            return minimum
+        else:
+            return max(interval2, key=lambda x: x.eval)
+
     def sum_eval(self) -> float:
         sum = 0
         for individual in self.population:
             sum += individual.eval
         return sum
 
-    def select_parent(self, sum_eval:float) -> int:
-        parent = -1
-        value = np.random.rand() * sum_eval
-        sum = 0
-        i = 0
-        while i < len(self.population) and sum < value:
-            sum += self.population[i].eval
-            parent += 1
-            i += 1
-        return parent
+    def select_parent(self, size:int) -> int:
+        return np.random.randint(size)
 
     def visualize_generation(self) -> None:
         best = self.population[0]
@@ -59,59 +60,53 @@ class GeneticAlgorithm:
 
         for i in tqdm(range(len(self.population))):
             self.population[i]._evaluate(self.X_train, self.X_test, self.y_train, self.y_test)
-        
-        self.sort_population()
 
-        self.best_solution = self.population[0]
-        self.solution_list.append(self.best_solution.eval)
-
-        self.visualize_generation()
 
         for i in range(generations):
-            print(f"Generation {i + 1}")
-            eval_sum = self.sum_eval()
-            new_population = []
-
-            for _ in range(0, self.population_size, 2):
-                parent1 = self.select_parent(eval_sum)
-                parent2 = self.select_parent(eval_sum)
-
-                children = self.population[parent1].crossover(self.population[parent2])
-
-                new_population.append(children[0].mutation(mutation_rate))   
-                new_population.append(children[1].mutation(mutation_rate))
-
-            self.population = list(new_population)
-
-            for i in tqdm(range(len(self.population))):
-                self.population._evaluate(self.X_train, self.X_test, self.y_train, self.y_test)
-            
-            pareto = []
             for individual in self.population:
                 frontier = not any((individual.loss > other_individual.loss and individual.eval < other_individual.eval) for other_individual in self.population)
 
                 if frontier:
-                    pareto.append(individual)
+                    self.pareto.append(individual)
+            self.frontier.append(self.pareto)
+            new_population = []
+            if len(self.pareto) == 1:
+                for _ in range(0, self.population_size, 2):
+                    parent1 = self.select_parent(len(self.pareto))
+                    parent2 = self.select_parent(self.population_size)
 
-            self.pareto.extend(pareto)
-            
+                    children = self.pareto[parent1].crossover(self.population[parent2])
 
-            self.sort_population()
-            self.visualize_generation()
+                    new_population.append(children[0].mutation(mutation_rate))
+                    new_population.append(children[1].mutation(mutation_rate))
+                    self.best_solution = self.pareto[0]
+            else:
+                for _ in range(0, self.population_size, 2):
+                    parent1 = self.select_parent(len(self.pareto))
+                    parent2 = self.select_parent(len(self.pareto))
 
-            self.solution_list.append(self.population[0].eval)
+                    children = self.pareto[parent1].crossover(self.pareto[parent2])
 
-            self.best_individual(self.population[0])
-        
+                    new_population.append(children[0].mutation(mutation_rate))
+                    new_population.append(children[1].mutation(mutation_rate))
+                    self.best_solution = self.best_individual()
+            print(f"Best Solution -> G:{self.best_solution.generation} -> ",
+              f"Eval: {round(self.best_solution.eval, 3)}",
+              f" Gene: {self.best_solution.gene}")
+            print(f"Generation {i + 1}")
+
+            self.population = list(new_population)
+
+            for i in tqdm(range(len(self.population))):
+                self.population[i]._evaluate(self.X_train, self.X_test, self.y_train, self.y_test)
+
+        self.frontier = [item for row in self.frontier for item in row]
+
         self.pareto_frontier = []
-        for individual in self.pareto:
-            frontier = not any((individual.loss > other_individual.loss and individual.eval < other_individual.eval) for other_individual in self.pareto)
+        for individual in self.frontier:
+            frontier = not any((individual.loss > other_individual.loss and individual.eval < other_individual.eval) for other_individual in self.frontier)
 
             if frontier:
                 self.pareto_frontier.append(individual)
 
-        print(f"Best Solution -> G:{self.best_solution.generation} -> ",
-              f"Eval: {round(self.best_solution.eval, 3)}",
-              f" Gene: {self.best_solution.gene}")
-        
         return self.best_solution
